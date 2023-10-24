0421e39c515b5db5d64b2ed04c1b047c1220e7ec
Kwenyerechukwu Paul <paulkwenye@gmail.com>
2023-10-24 14:19:10 +0300

    add children prop

--- src/App copy.js
+++ src/App copy.js
@@ -38,17 +38,25 @@ function Step() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <StepMessage step={step}>
+          <p className="message">
             Step {step}: {messages[step - 1]}
-          </StepMessage>
+          </p>
 
           <div className="buttons">
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
-              Previous <span>👈</span>
-            </Button>
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
-              Next <span>👉</span>
-            </Button>
+            <Button
+              bgColor="#7950f2"
+              textColor="#fff"
+              onClick={handlePrevious}
+              text="Previous"
+              emoji="👈"
+            />
+            <Button
+              bgColor="#7950f2"
+              textColor="#fff"
+              onClick={handleNext}
+              text="Next"
+              emoji="👉"
+            />
           </div>
         </div>
       )}
@@ -56,22 +64,13 @@ function Step() {
   );
 }
 
-function StepMessage({ step, children }) {
-  return (
-    <div className="message">
-      <h3>Step {step}</h3>
-      {children}
-    </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
+function Button({ textColor, bgColor, onClick, text, emoji }) {
   return (
     <button
       style={{ backgroundColor: bgColor, color: textColor }}
       onClick={onClick}
     >
-      {children}
+      <span>{emoji}</span> {text}
     </button>
   );
 }
